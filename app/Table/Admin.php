1f3762c1de3a0703b3c54e8a9b1a77b5c5c987f9
Zurma <12ab@gmx.fr>
2016-12-08 14:09:59 +0100

    add admin

--- app/Table/Admin.php
+++ app/Table/Admin.php
@@ -8,7 +8,15 @@ class Admin
 {
     public static function addAdmin($pseudo, $mdp, $email, $rank, $date=null) {
         if($date===null) $date=date("Y-m-d H:i:s");
-        $request = "INSERT INTO admin VALUES ($pseudo, $mdp, $email, $date, $rank);";
-        return App::getDatabase()->query($request, __CLASS__);
+        $statement = "INSERT INTO admin VALUES (?, ?, ?, ?, ?);";
+        $values = explode(", ","$pseudo, $mdp, $email, $date, $rank");
+        if(App::getDatabase()->prepare($statement,$values,Admin::class,"rowCount") == 1) return true;
+        return false;
+
+
+
+
+        /*$request = "INSERT INTO admin VALUES ($pseudo, $mdp, $email, $date, $rank);";
+        return App::getDatabase()->query($request, __CLASS__);*/
     }
 }
--- pages/admin.php
+++ pages/admin.php
@@ -1,16 +1,7 @@
-<?php
-use App\Table\Admin;
-if (isset($_POST['submit']))
-{
-    if(Admin::addAdmin($_POST['pseudo'],$_POST['mdp'],$_POST['email'],0)){
-
-    }
-}
-?>
 <div class="container">
     <div class="row">
         <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-            <form action="admin.php" method="post">
+            <form action="addAdmin.php" method="post">
                 <div class="form-group">
                     <label for="email">Email</label>
                     <input type="email" class="form-control" id="email" placeholder="Email">
